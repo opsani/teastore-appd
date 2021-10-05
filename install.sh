168b45f684507124544cdca0434b5fc1c9b2dab4
Robert Starmer <rstarmer@gmail.com>
2021-10-05 16:50:25 -0700

    Update loadgen2 (Jmeter) and install script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-#kubectl create -f https://raw.githubusercontent.com/DescartesResearch/TeaStore/development/examples/kubernetes/teastore-rabbitmq.yaml -n teastore
-#kubectl create -f https://raw.githubusercontent.com/DescartesResearch/TeaStore/development/examples/kubernetes/teastore-ribbon-kieker.yaml -n teastore
+brew install istioctl
+istioctl install -y
+
 kubectl create ns teastore
 kubectl label namespace teastore istio-injection=enabled --overwrite
 kubectl apply -f iks-teastore.yaml
